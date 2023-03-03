1382b424ca72fd0f323dca6968be3f2b4c3c55a3
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-03-03 07:47:13 -0300

    preparando ambiente

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 import MainLayoutProps from './MainLayoutProps'
-import Sidebar from '../components/Sidebar'
+
 
 export default function MainLayout(props: MainLayoutProps) {
   console.log(props)
@@ -30,20 +28,17 @@ export default function MainLayout(props: MainLayoutProps) {
         <div className="container text-center ">
           <div className="row">
             <div className="col">
-              <Navbar />
+
             </div>
           </div>
           <div className="row">
             <div className="col-lg-2 col-xl-2">
-              <Sidebar />
             </div>
-            <div className=" col-lg-10 px-4 col-xl-10">
               <main className="text-center ">{props.children}</main>
-            </div>
           </div>
         </div>
       </div>
-      <Footer />
+
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,8 @@ import MainLayout from '../layout/MainLayout'
 
 const Home: NextPage = () => {
   return (
-    <MainLayout title="Refeitório IFPI Picos">
-      <h1 className={styles.title}>Refeitório IFPI Picos</h1>
-      <p className={styles.title}>
-        Sistema de gerenciamento de refeitórios do IFPI - Picos - 2022
-      </p>
+    <MainLayout title="CodeChella - 2023">
+      <h1 className={styles.title}>Teste</h1>
     </MainLayout>
   )
 }
