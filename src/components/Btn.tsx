99118a3833e7cf5110bc21c5bcc9ada0aa35413d
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-03-04 18:08:37 -0300

    finalizando secao de datas do evento

---
+++ src/components/Btn.tsx
@@ -0,0 +1,9 @@
+export default function Btn() {
+  return (
+    <div className={`py-2`}>
+      <button type="button" className="btn btn-primary btn-lg">
+        Comprar ingresso!
+      </button>
+    </div>
+  )
+}
--- src/components/DateSection.tsx
+++ src/components/DateSection.tsx
@@ -1,3 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from '../styles/Home.module.css'
+import Btn from './Btn'
+
 export default function DateSection() {
-  return <div></div>
+  return (
+    <div
+      className={`px-3 d-flex flex-column  w-100 flex-lg-row justify-content-center py-4 ${styles.secaoDatas__container}`}
+    >
+      <div className={` mx-5 py-4 ${styles.secaoDatas__containerImg}`}>
+        <img
+          src="./img/home/imghome1.png"
+          className={` rounded-4 ${styles.secaoDatas__img}`}
+          alt="Imagem de fundo da Aurora Boreal"
+        />
+      </div>
+      <div className={`d-flex flex-column align-items-center}`}>
+        <div
+          className={`py-2 align-self-center ${styles.secaoDatas__ContainerTitulo}`}
+        >
+          <h2 className={`${styles.secaoDatas__titulo} text-break text-white`}>
+            {' '}
+            11 e 12 de Março Aluródromo de São Paulo{' '}
+          </h2>
+        </div>
+        <span
+          className={`${styles.secaoDatas__texto} align-self-center text-white`}
+        >
+          Hora de programar nossa memória com novas lembranças! Uma nova
+          experiência sobre música, linguagens e, claro, tecnologia! Somos um
+          festival diverso, com vários artistas e referências. Divirta-se!
+        </span>
+        <Btn />
+      </div>
+    </div>
+  )
 }
