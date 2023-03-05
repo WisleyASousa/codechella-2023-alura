47921ec615d2f43c6dd2f2f7675f4ac9ee61c974
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-03-05 10:13:07 -0300

    finalizando botao

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from '../styles/Home.module.css'
+
 export default function Btn() {
   return (
-    <div className={`py-2`}>
-      <button type="button" className="btn btn-primary btn-lg">
+    <div className={`py-2 d-flex justify-content-center`}>
+      <button
+        type="button"
+        className={`btn btn-primary gap-3 d-flex align-items-center btn-lg ${styles.btnIngresso}`}
+      >
         Comprar ingresso!
+        <img
+          src="./svgs/btn-simbol.svg"
+          className={``}
+          alt="Ícone de ingresso"
+        />
       </button>
     </div>
   )
