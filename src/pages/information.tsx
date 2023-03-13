f494c30106c1420349de9055cc39c968f460577c
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-03-13 11:22:28 -0300

    iniciando pagina de Informacoes Gerais

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next'
 import MainLayout from '../layout/MainLayout'
 import React from 'react'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.css'
 
 const Information: NextPage = () => {
-  return <MainLayout title="CodeChella - 2023"></MainLayout>
+  return (
+    <MainLayout title="CodeChella - 2023">
+      <Header
+        src="./img/ingressos/banner.png"
+        alt="Multidão em um festival"
+        text="Informações Gerais"
+        className={`${styles.Img__experienceBanner}`}
+      />
+      <div>
+        <span className={`${styles.tituloBodyInformation} text-white`}>
+          Perguntas Frequentes:
+        </span>
+      </div>
+    </MainLayout>
+  )
 }
 
 export default Information
